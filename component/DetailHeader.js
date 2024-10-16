5ea4581d183f8a5960bc678f49a7eed052d872da
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-10-16 12:13:38 +0900

    menu link

--- component/DetailHeader.js
+++ component/DetailHeader.js
@@ -4,18 +4,29 @@ import { ImgPath } from '../ImgPath';
 import { Pressable } from 'react-native';
 import { Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useMenu } from '../MenuProvider';
 
-const DetailHeader = ({ menuItems, menuActive, setMenuActive }) => {
-
-  const navigation = useNavigation();
+const DetailHeader = ({ menuItems }) => {
+  
+    const navigation = useNavigation();
+    const { menuActive, setMenuActive } = useMenu(); 
 
   return (
     <View style={[styles.DetailHeaderWrap]}>
       <View style={styles.titleWrap}>
-        <Pressable style={styles.backBtn} onPress={() => navigation.goBack()} >
+        <Pressable style={styles.backBtn} 
+          onPress={() =>{ 
+            setMenuActive('menu'); 
+            navigation.navigate('menu')
+          }}
+        >
           <Image source={ImgPath.back_arrow}/>
         </Pressable>
-        <Text>{menuActive}</Text>
+        <Text style={styles.title}>
+          {menuActive === 'mypet' && '설정'}
+          {menuActive === 'write' && '글쓰기'}
+          {menuActive === 'detail' && '상세'}
+        </Text>
       </View>
     </View>
   )
@@ -23,67 +34,29 @@ const DetailHeader = ({ menuItems, menuActive, setMenuActive }) => {
 
 const styles = StyleSheet.create({
   DetailHeaderWrap: {  
-      flexDirection: 'row',  
-      width: '100%',
-      padding: 20,
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      backgroundColor: '#fff',
-      zIndex: 999,
-      borderBottomWidth: 1,
-      borderColor: '#E7E9ED'
-    },
-    CustomHeaderWrapDetail: {
-      borderBottomWidth: 4,
-    },
-    CustomHeaderDetailWrap: {
-      justifyContent: 'center',
-      position: 'relative'
-    },
-    titleWrap: {
-      flexDirection: 'row',
-      alignItems: 'flex-end'
-    },
-    title: {
-      fontSize: 20,
-      lineHeight: 24,
-      fontWeight: '700',
-      marginRight: 8
-    },
-    desc: {
-      fontSize: 12,
-      color: '#8D96A4'
-    },
-    alarmWrap: {
-      position: 'relative',
-      justifyContent: 'center'
-    },
-    alarmUnread: {
-      width: 10,
-      height: 10,
-      borderWidth: 2,
-      borderColor: '#fff',
-      backgroundColor: '#EE815E',
-      position: 'absolute',
-      right: 1,
-      top: 4,
-      borderRadius: '50%',
-    },
-    backBtn: {
-      position: 'absolute',
-      left: 0,
-      top: 0
-    },
-    rowWrap: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      gap: 16
-    },
-    menuText: {
-      color: '#8D96A4',
-      fontSize: 18,
-      fontWeight: '700'
-    }
-  });
+    width: '100%',
+    padding: 20,
+    backgroundColor: '#fff',
+    zIndex: 999,
+    borderBottomWidth: 1,
+    borderColor: '#E7E9ED',
+    position: 'relative',
+  },
+  titleWrap: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  backBtn: {
+    position: 'absolute',
+    left: 0,
+    top: -5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#1F2329'
+  }
+});
   
 export default DetailHeader
