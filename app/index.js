800763486bea25405ac1bef5a63c3fc29a649dae
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-10-30 15:18:35 +0900

    font

--- app/index.js
+++ app/index.js
@@ -1,10 +1,26 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "../Navigation";
+import {setCustomText} from 'react-native-global-props';
 
 export default function Page() {
+
+  const customTextProps = {
+    style: {
+      fontFamily: 'WantedSans-Regular',
+    },
+  };
+
+  setCustomText(customTextProps);
+
+  const customTheme = {
+    colors: {
+      color: '#1F2329',
+    },
+  };
+
   return (
-    <NavigationContainer independent={true}>
+    <NavigationContainer independent={true} theme={customTheme}>
       <Navigation />
     </NavigationContainer>
-  );x
+  );
 }
